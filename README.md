f29cf0a3cf16a70e36b2660813532ef037934f43
wangytangy <yue.bruce.wang@gmail.com>
2017-03-22 15:36:41 -0400

    Add readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# Website Visit Tracker
+
+This Javascript API tracks the number of visits to an app in a given time frame
+and returns the number of visits in constant O(1) time.
+
+## Initialize a Tracker
+To initialize a tracker, call the constructor function passing in an integer that represents how many minutes ago you want to track:
+
+```js
+let tracker = Tracker.new(1;)
+// tracker will return number of visits 1 minute ago
+
+let trackerFiveMinAgo = Tracker.new(3);
+// tracker will return number of visits 3 minutes ago
+
+let trackerFiveMinAgo = Tracker.new();
+// Default: will return number of visits 5 minutes ago
+```
+By default, a tracker will track number of visits 5 minutes ago.
+
+### logHit()
+
+The function `logHit()` increments a count of total visits every time a user visits the app.
+`logHit()` must be invoked automatically when a hit on our app is detected.
+
+### getHits()
+The function `getHits()` that returns the total number of visits
+from a given number of minutes ago.
+
+```js
+let tracker = Tracker.new(1);
+
+let numberOfVisits = tracker.getHits();
+```
--- get_hits_five_min_ago.js
+++ get_hits_five_min_ago.js
@@ -6,10 +6,10 @@ Premise
 We have a web app that users are visiting. We need a function logHit()
 that increments a count of total visits every time a user visits the app.
 
-Assume logHit() is automatically when a hit on our app is detected.
+Assume logHit() is called automatically when a hit on our app is detected.
 
 Now we need a function getHits() that returns the total number of visits
-a given number of minutes ago.
+from a given number of minutes ago.
 
 */
 
@@ -18,7 +18,8 @@ a given number of minutes ago.
 class HitsTracker {
   // new HitsTracker obj can take an integer for calculating visits X min ago
   // (assigned 5 mins by default)
-  // for example, to track website visits 2 min ago => `let tracker = new HitsTracker(2)`;
+  // for example, to track website in the past 2 minutes => `let tracker = new HitsTracker(2)`;
+
   constructor(minAgo = 5) {
     // startPointer will keep track of the time of the least recent hits
     this.startPointer = null;
@@ -31,7 +32,7 @@ class HitsTracker {
     this.minsAgoInMilli = this.minsAgo * 60 * 1000;
 
     let ctx = this;
-    this.cleanHitsInterval = setInterval(function() { if (ctx.cleanHits) { ctx.cleanHits()}}, 1)
+    this.cleanHitsInterval = setInterval(function() { if (ctx._cleanHits) { ctx._cleanHits()}}, 1)
   }
 
   logHit() {
@@ -52,12 +53,12 @@ class HitsTracker {
   }
 
   /**
-    Cleanhits will be called every millisecond to constantly update our
+    _cleanhits will be called every millisecond to constantly update our
     hitsCount["totalCount"]. It will delete website hits older than 5 min ago.
 
     Startpoint gets set only ONCE by log_hit, at our first hit.
 
-    Every time cleanHits() is invoked, it will check if our Startpointer
+    Every time _cleanHits() is invoked, it will check if our Startpointer
     is at a longer time than 5 minutes ago.
 
     If yes, it means we have old logged hit entries that we don't want
@@ -66,11 +67,11 @@ class HitsTracker {
     StartPointer will be incremented every millisecond up until it
     reaches the 5 minutes ago mark.
 
-    1) cleanHits will delete those logged hits from hitsCount.
+    1) _cleanHits will delete those logged hits from hitsCount.
     2) It will also subtract those out of date logged hits from total hits.
 
   **/
-  cleanHits() {
+  _cleanHits() {
     let now = Date.now();
     let xMilliSecondsAgo = now - this.minsAgoInMilli;
 
@@ -95,7 +96,7 @@ class HitsTracker {
   }
 
   getHits() {
-    this.cleanHits();
+    this._cleanHits();
     return hitsCount["totalCount"];
   }
 
@@ -103,13 +104,6 @@ class HitsTracker {
   _toHHMMSS(ms) {
     return new Date(ms).toISOString().slice(11, -1);
     // console.log( time(1234567890) );  // "06:56:07.890"
-  }
-
-  simulateHits() {
-    // use setInterval (every second?) to logHit() a random number of times
-      // generate a random number
-      // invoke logHit() that number of times
-    // invoke in constructor
   }
 
 }
